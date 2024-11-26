a2cee259130d74732e866321936c17b9da7287f8
hildanku <hildankutomo@gmail.com>
2024-11-27 00:21:49 +0700

    feat: new config for svelte

---
+++ lua/plugins/svelte.lua
@@ -0,0 +1,14 @@
+-- lua/plugins/svelte.lua
+return {
+  -- Svelte syntax highlighting plugin
+  {
+    "evanleck/vim-svelte",
+    branch = "main",
+    dependencies = {
+      "pangloss/vim-javascript", -- for JavaScript syntax
+      "othree/html5.vim", -- for HTML syntax
+    },
+    ft = "svelte",
+  },
+}
+	
